42e0dc23092925a27951c1ab12a70cc85cfa28f3
sernapallyanurag <anurag.s@sonata-software.com>
2016-11-16 12:17:40 +0530

    build report

--- Rapid_cloud/app.js
+++ Rapid_cloud/app.js
@@ -199,6 +199,6 @@ var url = "http://172.29.59.65:3001/add";
 
 	});*/
 
-http.createServer(app).listen(app.get('port'), "172.29.59.62", function(){
+http.createServer(app).listen(app.get('port'), "172.29.59.44", function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
