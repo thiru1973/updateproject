8dd5d1a23589d1146ef4dd1ab8cca4d78dbb53c2
sernapallyanurag <anurag5348@hotmail.com>
2017-04-04 14:46:16 +0530

    Delete azurecred.ps1

--- Rapid_cloud/res_scripts/azurecred.ps1
+++
@@ -1,6 +0,0 @@
-$Username = "test@rapiddirectory.onmicrosoft.com"
-$Password = "Boron12#4"
-$SecPassword = Convertto-SecureString -String $password -AsPlainText -Force
-$Credential = New-object System.management.Automation.PSCredential $Username, $SecPassword
-
-Add-AzureAccount -Credential $Credential
